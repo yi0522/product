f09a1c65a09d2d8d4400bea3d646f2fd292b68c9
yi0522 <99787135+yi0522@users.noreply.github.com>
2022-05-24 15:35:56 +0800

    Add files via upload

---
+++ products.py
@@ -0,0 +1,49 @@
+import os
+#讀取檔案
+def read_file(filename):
+    produsts = []
+    with open ('products.csv','r',encoding='utf-8') as f:
+             for line in f:
+                if '商品,價格' in line:
+                    continue
+                name, price = line.strip().spilt(',')
+                products.append([name, price])
+    return products
+
+#
+def user_input(products):
+    while ture:
+        name = input('請輸入商品名稱:')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格:')
+        price = int(price)
+        products.append([name,price])
+    print(products)
+    return products
+
+#印出所有商品紀錄
+def print_products(products):
+    for p in products:
+        print(p[0],'的價格是',p[1])
+
+#寫入檔案
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:
+            f.write('商品,價格\n')
+            for p in products:
+                f.write(p[0] + ',' + str(p[1])+ '\n') 
+
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):#檢查檔案在不在
+        print('yeah!,有檔案')
+        products = read_file(filename)
+    else:
+        print('找不到檔案')
+    procucts = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main ()
